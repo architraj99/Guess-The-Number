aed224f562fa0a929ee0187711c42501e89b41e1
architraj99 <rajarchit99@gmail.com>
2026-05-19 00:07:12 +0530

    Made basic guess the number game UI

---
+++ script.js
@@ -0,0 +1,7 @@
+let guessInput = document.getElementById("guessInput");
+
+let guessBtn = document.getElementById("guessBtn");
+
+let message = document.querySelector(".message");
+
+let attempts = document.querySelector(".attempts");
