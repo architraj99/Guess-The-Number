2526eee3ee3d9f87b99e37836901eba56385dc41
architraj99 <rajarchit99@gmail.com>
2026-05-19 19:34:59 +0530

    Added random number logic and guess checking
    Made the game logic including random number generation, and  feedback messages for correct, high, and low guesses.

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 let guessInput = document.getElementById("guessInput");
-
 let guessBtn = document.getElementById("guessBtn");
-
 let message = document.querySelector(".message");
-
 let attempts = document.querySelector(".attempts");
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let totalAttempts = 0;
+
+guessBtn.addEventListener("click", function() {
+
+    let userGuess = Number(guessInput.value);
+    totalAttempts++;
+    attempts.innerText = "Attempts: " + totalAttempts;
+
+    if(userGuess === randomNumber) {
+
+        message.innerText = "Correct Number!";
+    }
+    else if(userGuess > randomNumber) {
+
+        message.innerText = "Too High!";
+    }
+    else {
+
+        message.innerText = "Too Low!";
+    }
+}
+);
