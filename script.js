0989bbda9b6e99a2ce7aef27a5b3ea7ec5c632f4
architraj99 <rajarchit99@gmail.com>
2026-05-19 22:35:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,72 @@
-let guessInput = document.getElementById("guessInput");
-let guessBtn = document.getElementById("guessBtn");
+let guessInput =
+    document.getElementById("guessInput");
+
+let guessBtn =
+    document.getElementById("guessBtn");
+
+let restartBtn =
+    document.getElementById("restartBtn");
+
 let message = document.querySelector(".message");
 let attempts = document.querySelector(".attempts");
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 let totalAttempts = 0;
+let maxAttempts = 10;
+let gameOver = false;
 
-guessBtn.addEventListener("click", function() {
+guessBtn.addEventListener("click", function () {
 
-    let userGuess = Number(guessInput.value);
+        if (gameOver) {
+            return;
+        }
 
-    if(!userGuess) {
+        let userGuess = Number(guessInput.value);
 
-        message.innerText = "Enter a valid number";
-        return;
+        if (!userGuess) {
+
+            message.innerText =" Enter a valid number";
+            return;
+        }
+
+        totalAttempts++;
+
+        attempts.innerText = "Attempts: " + totalAttempts + "/ " + maxAttempts;
+
+        if (userGuess === randomNumber) {
+
+            message.innerText = "Correct Number!";
+            message.style.color = "#16a34a";
+            gameOver = true;
+            restartBtn.style.display = "block";
+        }
+
+        else if (userGuess > randomNumber) {
+
+            message.innerText = "📈 Too High!";
+            message.style.color = "#dc2626";
+        }
+
+        else {
+
+            message.innerText = "📉 Too Low!";
+            message.style.color = "#ea580c";
+        }
+
+        if (totalAttempts >= maxAttempts && userGuess !== randomNumber) {
+
+            message.innerText = "You Lost... Number was " + randomNumber;
+
+            message.style.color = "#dc2626";
+            gameOver = true;
+            restartBtn.style.display = "block";
+        }
+
+        guessInput.value = "";
     }
+);
 
-    totalAttempts++;
-    attempts.innerText = "Attempts: " + totalAttempts;
+restartBtn.addEventListener("click", function () {
 
-    if(userGuess === randomNumber) {
-
-        message.innerText = "Correct Number!";
-        message.style.color = "#16a34a";
+        location.reload();
     }
-    else if(userGuess > randomNumber) {
-
-        message.innerText = "Too High!";
-        message.style.color = "#dc2626";
-    }
-    else {
-
-        message.innerText = "Too Low!";
-        message.style.color = "#ea580c";
-    }
-
-    guessInput.value = "";
-}
 );
